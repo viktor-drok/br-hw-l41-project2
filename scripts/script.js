a59df1724f6678f8de6f170b0c1a5d7e8793ff1a
viktor-drok <vityadrok@gmail.com>
2022-09-04 12:51:03 +0300

    add content

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
 new Swiper(".hero__swiper", {
     direction: "vertical",
     pagination: {
-        el: ".swiper-pagination",
+        el: ".hero__swiper .swiper-pagination",
         clickable: true,
+    },
+});
+
+new Swiper(".news-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".news-swiper .swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".news-swiper .swiper-button-next",
+        prevEl: ".news-swiper .swiper-button-prev",
     },
 });
